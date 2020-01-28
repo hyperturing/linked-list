4a6c1105c822e52b6c257a52f08d5f3c19577f82
Ash Peterson <ashnight07@gmail.com>
2020-01-28 08:50:49 -0600

    Basic list accessor methods added

--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,7 @@
 require_relative 'node'
 
 class LinkedList
-  attr_accessor :head, :tail
+  attr_accessor :tail
   def initialize
     @head = nil
     @tail = nil
@@ -23,5 +23,35 @@ class LinkedList
       @start = @start.next_node
     end
     string + ' nil'
+  end
+
+  def size
+    @size = 0
+    @start = @head
+    until @start.nil?
+      @size += 1
+      @start = @start.next_node
+    end
+    @size
+  end
+
+  def head
+    @head.value
+  end
+
+  def tail
+    @start = @head
+    @start = @start.next_node until @start.next_node.nil?
+    @start.value
+  end
+
+  def at(index)
+    @location = 0
+    @start = @head
+    until @start.nil? || @location == index
+      @location += 1
+      @start = @start.next_node
+    end
+    @start.nil? ? -1 : @start.value
   end
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
 require_relative 'linked_list'
 
 list = LinkedList.new
+list.prepend(4)
+list.prepend(3)
 list.prepend(2)
 list.prepend(1)
+
+#puts list.head
+#puts list.tail
+puts list.at(2)
 puts list
