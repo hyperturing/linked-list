ecf170fdc78085dcff35c9f00f3f5b9f300353e2
Ash Peterson <ashnight07@gmail.com>
2020-01-28 13:21:55 -0600

    Improved tester readability

--- main.rb
+++ main.rb
@@ -10,23 +10,31 @@ list.prepend(1)
 list.append(5)
 
 puts "List head is #{list.head}"
-puts "List tail is #{list.tail}"
-puts list
+puts "List tail is #{list.tail}\n\n"
+
+puts 'Our list:'
+puts "#{list}\n\n"
 puts "Popping item #{list.pop} out of list"
-puts list
+puts 'Our list now:'
+puts "#{list}\n\n"
 
 # A value and location to test list accessor and mutation methods
 value = 6
 location = 1
 
-puts list.at(5)
-puts list.contains(value)
-puts list.find(value)
+puts "Locating value at location #{location}"
+puts "#{list.at(location)}\n\n"
+puts "Does our list contain #{value}?"
+puts "#{list.contains(value)}\n\n"
+puts "Finding the location of #{value} in our list:"
+puts "#{list.find(value)}\n\n"
 
 puts "Inserting #{value} at location #{location}"
 list.insert_at(value, location)
-puts list
+puts 'Our list now:'
+puts "#{list}\n\n"
 
 puts "Removing item at location #{location} "
 list.remove_at(location)
-puts list
+puts 'Our list now:'
+puts "#{list}\n\n"
