f723e28e09a5e7220ac3ac7c9e14647732f157de
Ash Peterson <ashnight07@gmail.com>
2020-01-28 00:11:36 -0600

    Basic prepend function and string output function

--- linked_list.rb
+++ linked_list.rb
@@ -6,4 +6,22 @@ class LinkedList
     @head = nil
     @tail = nil
   end
+
+  def prepend(value)
+    if @head.nil?
+      @head = Node.new(value, @tail)
+    else
+      @head = Node.new(value, @head)
+    end
+  end
+
+  def to_s
+    string = ''
+    @start = @head
+    until @start.nil? do
+      string << " ( #{@start.value} ) ->"
+      @start = @start.next_node
+    end
+    string + ' nil'
+  end
 end
